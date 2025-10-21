49a602e8a09c36c01d9a4173df4841518ae975fc
Silambarasan20 <134932524+Silambarasan20@users.noreply.github.com>
2025-10-21 15:07:42 +0530

    Update variables.tf

---
+++ aws-Observability-terraform/src/variables.tf
@@ -0,0 +1,41 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "12.0.0.0/16"
+}
+
+variable "azs" {
+  description = "List of availability zones for the VPC"
+  type        = list(string)
+  default     = ["us-east-1a"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access. Must exist in the target region."
+  type        = string
+  default     = "keypair"
+}
+
+variable "my_ip_cidr" {
+  description = "Your IP address(Personal laptop) in CIDR notation used for locking down access to management ports."
+  type        = string
+  default     = "xxx.xxx.xx.xxx/32"
+}
+
+variable "custom_ip_cidr" {
+  description = "A custom IP address/CIDR to allow access Prometheus and Grafana server IP."
+  type        = string
+  default     = "xx.xx.xxx.xx/32"
+}
+
+variable "Terraform_ip_cidr" {
+  description = "A Terraform instance IP address/CIDR to allow access."
+  type        = string
+  default     = yy.yyy.yyy.yy/32"
+}
